f481d41d94977274920fe67cccb5e3aca90c95d0
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-04-12 15:00:41 +0300

    Lion Name Lion Age

---
+++ Ex2-HelloVSCode/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Please enter your name: ");
+string lionName = Console.ReadLine();
+
+Console.WriteLine("Please enter your age: ");
+string lionAge = Console.ReadLine();
+
+Console.WriteLine("Hello " + lionName + " your age is " + lionAge);
