997e2a18d719ed13ed15ad3eab1282c605c4fac0
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-04-12 14:47:23 +0300

    Lion Name City Country

---
+++ Ex1-HelloVisualStudio/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please enter your city: ");
+string city = Console.ReadLine();
+
+Console.WriteLine("Please enter your country: ");
+string country = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + " you are born in " + country + " in the city of " + city + ".");
