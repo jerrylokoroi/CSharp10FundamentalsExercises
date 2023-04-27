9d99d3b51ed1a5e4c4c82e4932952e6a409778fa
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-04-27 18:54:41 +0300

    Name Favourite Movie

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,18 +1,4 @@
-﻿/*
-  
-Console.WriteLine("What is your name?");
-string name = Console.ReadLine();
-
-Console.WriteLine("How old are you?");
-int age = int.Parse(Console.ReadLine());
-
-Console.WriteLine("How tall are you?");
-double height = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Hi " + name + ", you are " + age + " years old and " + height + " feet tall.");
-
-*/
-Console.WriteLine("What is your name ?");
+﻿Console.WriteLine("What is your name ?");
 string name = Console.ReadLine();
 Console.WriteLine("Which movie is your favourite ?");
 string movie = Console.ReadLine();
