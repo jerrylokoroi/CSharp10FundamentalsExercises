d5f109b997a1fbbdd212af83b5e4a9f8db73b98d
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-05-03 23:08:15 +0300

    Implicit and Explicit

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,2 +1,7 @@
-﻿DateTime dt = new DateTime(2023, 5, 3);
-Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
+﻿// Implicit 
+int a = 1233455;
+float l = a;
+
+// Explicit 
+double d = 1213242;
+int j = (int)d;
