e02b46217cb4258bc8651466e0434b30cc7f34a9
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-05-03 23:41:40 +0300

    Two Numbers Use Operators

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,7 +1,10 @@
-﻿// Implicit 
-int a = 1233455;
-float l = a;
+﻿double ratePerHour = 125.50;
+int numOfHoursWorked = 95;
+int bonus = 1000;
+double currentMonthStipend = ratePerHour * numOfHoursWorked + bonus;
 
-// Explicit 
-double d = 1213242;
-int j = (int)d;
+ratePerHour -= 25;
+Console.WriteLine(currentMonthStipend);
+
+double currentMonthStipendInPercentage = currentMonthStipend / 100;
+Console.WriteLine(currentMonthStipendInPercentage);
