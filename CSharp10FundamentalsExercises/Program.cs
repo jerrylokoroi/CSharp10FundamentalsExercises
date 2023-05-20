007422672490f2c99b0d7bde3b9ce8772ac213db
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-05-20 10:28:17 +0300

    Switch Case = Day of Week

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,16 +1,41 @@
 ﻿
-Console.WriteLine("Enter a letter: ");
-char letter = Console.ReadLine().ToLower()[0];
-
-string result = (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
-    ? "Vowel"
-    : "Consonant";
-
-Console.WriteLine(result);
 
 
+Console.WriteLine("Enter a number between 1 and 7: ");
+string selectedNumber = Console.ReadLine();
 
 
+if (selectedNumber == "1")
+{
+    Console.WriteLine("Monday");
+}
+else if (selectedNumber == "2")
+{
+    Console.WriteLine("Tuesday");
+}
+else if(selectedNumber == "3")
+{
+    Console.WriteLine("Wednesday");
+}else if (selectedNumber == "4")
+{
+    Console.WriteLine("Thursday");
+}
+else if (selectedNumber == "5")
+{
+    Console.WriteLine("Friday");
+}
+else if (selectedNumber == "6")
+{
+    Console.WriteLine("Saturday");
+}
+else if (selectedNumber == "7")
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
 
 
 
