6561b1fc87557e761e53b9776b0cbaf4c336b964
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-05-20 11:04:27 +0300

    While Loop 1 to enteredNumber

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,32 +1,9 @@
-﻿
+﻿Console.WriteLine("Enter a number: ");
+int enteredNumber = int.Parse(Console.ReadLine());
+int i = 1;
 
-
-Console.WriteLine("Enter a number between 1 and 7: ");
-string selectedNumber = Console.ReadLine();
-
-switch (selectedNumber) {
-    case "1":
-        Console.WriteLine("Monday");
-        break;
-    case  "2":
-    Console.WriteLine("Tuesday");
-        break;
-    case "3":
-    Console.WriteLine("Wednesday");
-        break;
-    case "4":
-    Console.WriteLine("Thursday");
-        break;
-    case "5":
-    Console.WriteLine("Friday");
-        break;
-    case "6":
-    Console.WriteLine("Saturday");
-        break;
-    case "7":
-    Console.WriteLine("Sunday");
-        break;
-    default:
-        Console.WriteLine("Invalid");
-        break;
-}
+while (enteredNumber >= i)
+{
+    Console.WriteLine(i);
+    i++;
+}   
