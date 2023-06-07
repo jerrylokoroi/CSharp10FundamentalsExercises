364761a067c7707cd3c9fed3ff15f7cd4fcf0780
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-07 13:02:24 +0300

    program that prompts the user for a decimal value and then converts it to a string with two decimal places and displays it.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,3 +1,5 @@
-﻿using CSharp10FundamentalsExercises;
+﻿Console.WriteLine("Enter a decimal value:");
+decimal input = Decimal.Parse(Console.ReadLine());
 
-Utility.Main();
+string formattedValue = input.ToString("0.00");
+Console.WriteLine("Formatted value: " + formattedValue);
