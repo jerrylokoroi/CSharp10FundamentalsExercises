902cf721bc9bc75f8c9dab612e86e704452dbf21
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-07 12:24:08 +0300

    Recognize User Name Even If Name MIspelled

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,11 +1,8 @@
-﻿using CSharp10FundamentalsExercises;
+﻿Console.WriteLine("Enter first name: ");
+string firstName = Console.ReadLine();
 
-Console.WriteLine("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("Enter the activiy:");
-string activity = Console.ReadLine();
-Console.WriteLine("Enter the number of average hours of workout per day:");
-string averageHoursPerDay = Console.ReadLine();
+Console.WriteLine("Enter last name: ");
+string lastName = Console.ReadLine();
 
-int totalAverageHoursPerWeek = Utility.CalcTotalNumOfHrsOfWorkoutPerWeek(averageHoursPerDay);
-Console.WriteLine($"Congratulations {name} ! You have practiced {activity} for {totalAverageHoursPerWeek} average hours this week. Congatulations!");
+
+Console.WriteLine($"Hello \n{firstName.ToUpper()}\t{lastName.ToUpper()}");
