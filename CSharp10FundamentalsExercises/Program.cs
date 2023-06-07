84892dbd2fa0af3a530c95fb8272d86a946c7bdb
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-07 12:40:49 +0300

    prints fristName, lastName, City and Street each on a new line

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -4,5 +4,13 @@ string firstName = Console.ReadLine();
 Console.WriteLine("Enter last name: ");
 string lastName = Console.ReadLine();
 
+Console.WriteLine("Enter city: ");
+string city = Console.ReadLine();
 
-Console.WriteLine($"Hello \n{firstName.Trim().ToUpper()} {lastName.Trim().ToUpper()}");
+Console.WriteLine("Enter street: ");
+string street = Console.ReadLine();
+
+Console.WriteLine("Enter PO box: ");
+int address = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"First Name: {firstName}\nLast Name: {lastName}\nCity: {city}\nStreet: {street}\nP.O. Box: {address}\n");
