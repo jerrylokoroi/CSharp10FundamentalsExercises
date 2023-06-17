54dcbff6bd288a318986273d5e34ebe3f7d23e1c
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-17 11:05:50 +0300

    Class named “Car” with 2 Fields

---
+++ CSharp10FundamentalsExercises/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp10FundamentalsExercises
+{
+    internal class Car
+    {
+        private string color;
+        private int maxSpeed;
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Engine Started!");
+        }
+
+
+    }
+}
--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,5 +1,1 @@
-﻿Console.WriteLine("Enter a decimal value:");
-decimal input = Decimal.Parse(Console.ReadLine());
-
-string formattedValue = input.ToString("0.00");
-Console.WriteLine("Formatted value: " + formattedValue);
+﻿
