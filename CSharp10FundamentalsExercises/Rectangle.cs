b018572b0b26024c1c4096245707bdcde902654b
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-18 12:56:44 +0300

    Create a class called "Rectangle"

--- CSharp10FundamentalsExercises/Rectangle.cs
+++ CSharp10FundamentalsExercises/Rectangle.cs
@@ -8,5 +8,28 @@ namespace CSharp10FundamentalsExercises
 {
     internal class Rectangle
     {
+        //{ get; } => Defines a property with a read-only accessor
+        public double Length { get; }
+        public double Width { get; }
+
+        public  Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public double CalculateArea(double length, double width)
+        {
+            double area = Length * Width;
+            // return length * width 
+            return area;
+        }
+
+        public double CalculatePerimeter(double length, double width)
+        {
+            double perimeter = 2 * (Length + Width);
+            // return 2 * (Length + Width)
+            return perimeter;
+        }
     }
 }
