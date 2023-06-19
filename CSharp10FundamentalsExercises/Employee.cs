d96b386f0754e86537e01f24f7e68ff3920461e9
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-19 11:01:33 +0300

    Create a class called "Employee"

---
+++ CSharp10FundamentalsExercises/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp10FundamentalsExercises
+{
+    public class Employee
+    {
+        public string name;
+        public int age;
+        public double salary;
+
+       
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int age, double salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public void EmployeeSalaryRaise(double raiseAmount)
+        {
+            salary += raiseAmount;
+        }
+
+        
+    }
+}
--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,2 +1,7 @@
-﻿
+﻿using CSharp10FundamentalsExercises;
 
+Employee employee = new("Jerry", 23, 30000);
+Console.WriteLine($"Initial Salary: {employee.Salary}");
+
+employee.GiveRaise(10000);
+Console.WriteLine($"New Salary after Raise: {employee.Salary}");
