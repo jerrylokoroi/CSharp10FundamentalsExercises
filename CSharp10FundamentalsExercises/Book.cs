de6507da833b56fc1fd7a34c53957c1a1c00d3e9
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-19 11:54:34 +0300

    "Book" with the following properties: Title, Author, and ISBN.

--- CSharp10FundamentalsExercises/Book.cs
+++ CSharp10FundamentalsExercises/Book.cs
@@ -8,5 +8,31 @@ namespace CSharp10FundamentalsExercises
 {
     internal class Book
     {
+        public string Title { get; }
+        public string Author { get; }
+        public string ISBN { get; }
+
+        public bool IsAvailable { get; private set; }
+
+        public Book(string title, string author, string isbn)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            IsAvailable = true; //Assuming the book is initially available
+        }
+
+        public void BorrowBook()
+        {
+            if (IsAvailable)
+            {
+                IsAvailable = false;
+                Console.WriteLine($"The book '{Title}' by {Author} has been borrowed.");
+            }
+            else
+            {
+                Console.WriteLine($"The book '{Title}' by {Author} is currently not available.");
+            }
+        }
     }
 }
