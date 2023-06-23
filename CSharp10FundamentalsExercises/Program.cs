5942e4175820450970bea41cb3cd0c7b81ea65c2
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-23 12:49:36 +0300

    Use the string.ToUpper() and string.ToLower() methods

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,6 +1,9 @@
-﻿string str1 = "Hello";
-string str2 = " World";
+﻿string string1 = "Jerry";
+string string2 = "Lokoroi";
 
-string result = String.Concat(str1, str2);
+string string4 = string1.ToLower();
+string string3 = string2.ToUpper();
 
-Console.WriteLine(result);
+
+Console.WriteLine(string3);
+Console.WriteLine(string4);
