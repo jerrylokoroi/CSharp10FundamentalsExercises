dcc1945436941bb8b651ccded5f0e243d4aac520
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-24 11:01:31 +0300

    Create a struct called "Rectangle"

--- CSharp10FundamentalsExercises/Rectangle2.cs
+++ CSharp10FundamentalsExercises/Rectangle2.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp10FundamentalsExercises
 {
-    internal class Rectangle2
+    internal struct Rectangle2
     {
+        public struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+        }
+
+        public struct Rectangle
+        {
+            public Point Left { get; set; }
+            public Point Right { get; set; }
+
+            public int CalculateArea()
+            {
+                int width = Math.Abs(Right.X - Left.X);
+                int height = Math.Abs(Right.Y - Left.Y);
+                return width * height;
+            }
+        }
+
     }
 }
