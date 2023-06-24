3d255bd127e666ad051485e9e61b3c43589fffa6
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-24 10:44:50 +0300

    Create an enum called "Months" with values of months and use it to write a method that takes a number (1-12) as input and returns the corresponding month name as a string

--- CSharp10FundamentalsExercises/Months.cs
+++ CSharp10FundamentalsExercises/Months.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace CSharp10FundamentalsExercises
 {
-    internal class Months
+    internal enum Months
     {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    public static string GetMonthName(int number)
+    {
+        if (Enum.IsDefined(typeof(Months), number))
+        {
+            Months month = (Months)number;
+            return month.ToString();
+        }
+
+        throw new ArgumentException("Invalid month number.");
     }
 }
