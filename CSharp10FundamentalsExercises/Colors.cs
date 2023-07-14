38c34808d9518a046e778435a3e817d5aa05c291
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-07-14 13:01:31 +0300

    Create a class called "Colors" and add static fields for common colors ("Red", "Green", "Blue")

---
+++ CSharp10FundamentalsExercises/Colors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp10FundamentalsExercises
+{
+    internal class Colors
+    {
+        public static  string Red = "Red";
+        public static  string Green = "Green";
+        public static  string Blue = "Blue";
+
+        public static ConsoleColor GetColor(string nameOfColor)
+        {
+            switch (nameOfColor)
+            {
+                case "red":
+                    return ConsoleColor.Red;
+                case "green":
+                    return ConsoleColor.Green;
+                case "blue":
+                    return ConsoleColor.Blue;
+                default:
+                    throw new ArgumentException("Invalid color name.");
+            }
+        }
+    }
+}
+
--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,15 +1,9 @@
 ﻿using CSharp10FundamentalsExercises;
-using CSharp10FundamentalsExercises.MyShapes;
 
-// Creating instances of each shape
-CSharp10FundamentalsExercises.Circle circle = new CSharp10FundamentalsExercises.Circle(5);
-Square square = new Square(4);
-CSharp10FundamentalsExercises.Rectangle rectangle = new CSharp10FundamentalsExercises.Rectangle(3, 6);
+string colorName = "Green";
+ConsoleColor color = Colors.GetColor(colorName);
 
-// Calculating and printing the areas
-Console.WriteLine("Circle area: " + circle.CalculateArea());
-Console.WriteLine("Square area: " + square.CalculateArea());
-Console.WriteLine("Rectangle area: " + rectangle.CalculateArea());
+Console.WriteLine($"Color name: {colorName}");
 
 
 
