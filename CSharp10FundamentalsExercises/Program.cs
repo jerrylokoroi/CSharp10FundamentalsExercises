71e0123550b4bb67e8516d04dbf28c983f3e80fb
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-08-08 14:34:25 +0300

    Create a two-dimensional array of integers and use nested loops to print out all elements in the array

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,16 +1,22 @@
-﻿// Create an array of doubles
-double[] values = { 2.5, 3.8, 1.7, 4.2, 5.0 };
+﻿// Create a 2D array of integers
+int[,] array2D = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
 
-// Calculate the sum of all elements
-double sum = 0.0;
-for (int i = 0; i < values.Length; i++)
+// Get the dimensions of the array
+int rows = array2D.GetLength(0);
+int cols = array2D.GetLength(1);
+
+// Print all elements in the array
+Console.WriteLine("Array elements:");
+for (int row = 0; row < rows; row++)
 {
-    sum += values[i];
+    for (int col = 0; col < cols; col++)
+    {
+        Console.Write(array2D[row, col] + " ");
+    }
+    Console.WriteLine(); // Move to the next row
 }
-
-// Calculate the average
-double average = sum / values.Length;
-
-// Print the average
-Console.WriteLine($"Average: {average}");
 
