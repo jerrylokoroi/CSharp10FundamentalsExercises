b52d680752666b07cdd39cbb21dca6a132af447e
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-08-25 13:04:59 +0300

    Create a simple class that represents a calculator, with methods for basic arithmetic operations (addition, subtraction, multiplication, division). Write unit tests that test each of these methods, testing for both expected inputs and expected outputs, as well as some edge cases (e.g. division by zero). Use a testing framework like MSTest, NUnit or XUnit.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,8 +1,1 @@
-﻿Console.Write("Enter a number: ");
-string userInput = Console.ReadLine();
-
-int parsedValue = int.Parse(userInput);  // Set breakpoint here
-
-int result = parsedValue * 2;
-
-Console.WriteLine($"The result of doubling the parsed value {parsedValue} is {result}");
+﻿
---
+++ CalculatorTests/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorTests
+{
+    internal class Calculator
+    {
+        internal int Add(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double Divide(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int Multiply(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int Subtract(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
