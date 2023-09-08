f717be23e4f76411492ed8eaee416830bea8bc6d
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-08 11:07:51 +0300

    This is a commit messege

---
+++ CSharp10FundamentalsExercises/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp10FundamentalsExercises
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Division by zero is not allowed.");
+            }
+
+            return (double)a / b;
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,60 @@
+using CSharp10FundamentalsExercises;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Add_WhenGivenTwoIntegers_ReturnsSum()
+        {
+            Calculator calculator = new Calculator();
+
+            int result = calculator.Add(3, 5);
+
+            Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Subtract_WhenGivenTwoIntegers_ReturnsDifference()
+        {
+            Calculator calculator = new Calculator();
+
+            int result = calculator.Subtract(10, 4);
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Multiply_WhenGivenTwoIntegers_ReturnsProduct()
+        {
+            Calculator calculator = new Calculator();
+
+            int result = calculator.Multiply(6, 4);
+
+            Assert.That(result, Is.EqualTo(24));
+        }
+
+        [Test]
+        public void Divide_WhenDividingByNonZero_ReturnsQuotient()
+        {
+            Calculator calculator = new Calculator();
+
+            double result = calculator.Divide(10, 2);
+
+            Assert.That(result, Is.EqualTo(5.0).Within(0.001));
+        }
+
+        [Test]
+        public void Divide_WhenDividingByZero_ThrowsArgumentException()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.Throws<ArgumentException>(() => calculator.Divide(10, 0));
+        }
+    }
+}
