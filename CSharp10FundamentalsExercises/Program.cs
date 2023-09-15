b4810b63d8b93be2b2c6bfe1d5ec53d567d3afed
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-15 16:07:58 +0300

    Write a C# program that creates a new file called “output.txt”. Use the StreamWriter to write the text “Hello, World!” to the file.Close the StreamWriter and verify that the text was written to the file.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string filePath = @"C:\Users\Renter\Development\CSharp10FundamentalsExercises\CSharp10FundamentalsExercises\output.txt";
+
+using (StreamWriter writer = new StreamWriter(filePath))
+{
+    writer.WriteLine("Hello, World!");
+}
+
+string content = File.ReadAllText(filePath);
+
+if (content == "Hello, World!")
+{
+    Console.WriteLine("Text was successfully written to the file.");
+}
+else
+{
+    Console.WriteLine("Error: Text was not written to the file.");
+}
