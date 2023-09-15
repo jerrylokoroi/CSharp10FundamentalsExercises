fb4d4d7b3160fa2fddf4ddcb65bfec827ab31631
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-15 16:44:43 +0300

    Write a C# program that creates a new directory called “mydir”. Use Directory.CreateDirectory to create a subdirectory called “mysubdir” inside “mydir”. Use Directory.GetFiles to get a list of files in “mydir”. Print out the names of the files to the console.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,21 +1,17 @@
-﻿string filePath = "output.bin";
+﻿// Create the main directory "mydir"
+string mainDirectory = "mydir";
+Directory.CreateDirectory(mainDirectory);
 
-// Write integers to the binary file
-using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+// Create the subdirectory "mysubdir" inside "mydir"
+string subDirectory = Path.Combine(mainDirectory, "mysubdir");
+Directory.CreateDirectory(subDirectory);
+
+// Get a list of files in "mydir"
+string[] filesInMyDir = Directory.GetFiles(mainDirectory);
+
+// Print out the names of the files
+Console.WriteLine("Files in mydir:");
+foreach (string file in filesInMyDir)
 {
-    writer.Write(1);
-    writer.Write(2);
-    writer.Write(3);
-    writer.Write(4);
-}
-
-// Read integers from the binary file
-using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
-{
-    int num1 = reader.ReadInt32();
-    int num2 = reader.ReadInt32();
-    int num3 = reader.ReadInt32();
-    int num4 = reader.ReadInt32();
-
-    Console.WriteLine($"{num1}, {num2}, {num3}, {num4}");
+    Console.WriteLine(Path.GetFileName(file));
 }
