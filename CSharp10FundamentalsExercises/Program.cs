0ed4f3b2d1995cc4e521a5027cef836f42f7c11d
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-15 17:03:43 +0300

    Create a console application that prompts the user for a file path, a string, and an integer. The program will then create a new file at the given path, write the string to the file n times, where n is the given integer. Use the System.IO namespace and the File and FileStream classes to write the file.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -2,25 +2,24 @@
 Console.Write("Enter the file path: ");
 string filePath = Console.ReadLine();
 
-// Check if the file exists
-if (File.Exists(filePath))
-{
-    // Read the file using FileStream and StreamReader
-    using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-    {
-        using (StreamReader reader = new StreamReader(fs))
-        {
-            string line;
+// Prompt user for string
+Console.Write("Enter the string to write: ");
+string inputString = Console.ReadLine();
 
-            // Read and print each line
-            while ((line = reader.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
+// Prompt user for integer
+Console.Write("Enter the number of times to write the string: ");
+int repeatTimes = Convert.ToInt32(Console.ReadLine());
+
+// Create a new file and write the string n times
+using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+{
+    using (StreamWriter writer = new StreamWriter(fs))
+    {
+        for (int i = 0; i < repeatTimes; i++)
+        {
+            writer.WriteLine(inputString);
         }
     }
 }
-else
-{
-    Console.WriteLine("File not found.");
-}
+
+Console.WriteLine($"File '{Path.GetFileName(filePath)}' created and written successfully.");
