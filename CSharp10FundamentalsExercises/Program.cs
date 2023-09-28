9c70d450aea44dd5f67ee97707fad1baa9991951
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-28 22:39:21 +0300

    Create a program that reads a file and displays its contents to the console. Use a try-catch block to catch any FileNotFound exception that may occur and display a message to the user.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,31 +1,23 @@
 ﻿try
 {
-    Console.Write("Enter a number: ");
-    int userInput = Convert.ToInt32(Console.ReadLine());
+    string filePath = @"C:\Users\Renter\Development\CSharp10FundamentalsExercises\CSharp10FundamentalsExercises\File_path\sample.txt";
 
-    int result = DivideByTen(userInput);
-    Console.WriteLine($"Result: {result}");
+    if (File.Exists(filePath))
+    {
+        string content = File.ReadAllText(filePath);
+        Console.WriteLine("File Content:");
+        Console.WriteLine(content);
+    }
+    else
+    {
+        Console.WriteLine("Error: The specified file does not exist.");
+    }
 }
-catch (DivideByZeroException ex)
+/*catch (FileNotFoundException ex)
 {
-    Console.WriteLine("Error: Cannot divide by zero. " + ex.Message);
-}
-catch (FormatException ex)
-{
-    Console.WriteLine("Error: Invalid input. " + ex.Message);
-}
+    Console.WriteLine("Error: File not found. " + ex.Message);
+}*/
 catch (Exception ex)
 {
     Console.WriteLine("An unexpected error occurred: " + ex.Message);
-}
-    
-
-    static int DivideByTen(int number)
-{
-    if (number == 0)
-    {
-        throw new DivideByZeroException();
-    }
-
-    return 10 / number;
 }
