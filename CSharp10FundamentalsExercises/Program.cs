80f9f4a8e59a89cf29fdfb95ba7222bea5f47ae5
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-28 23:25:50 +0300

    Create a program that takes two strings as input from the user and concatenate them together. Use Exception handling to catch any errors that might occur during the concatenation operation. If an error occurs, output a message to the user explaining the error.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,35 +1,22 @@
-﻿Random random = new Random();
-int targetNumber = random.Next(1, 101);
-
-bool guessedCorrectly = false;
-
-try
+﻿try
 {
-    while (!guessedCorrectly)
-    {
-        Console.Write("Guess the number (between 1 and 100): ");
-        int userGuess = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the first string: ");
+    string firstString = Console.ReadLine();
 
-        if (userGuess == targetNumber)
-        {
-            guessedCorrectly = true;
-            Console.WriteLine("Congratulations! You guessed the number.");
-        }
-        else if (userGuess < targetNumber)
-        {
-            Console.WriteLine("Too low! Try guessing a higher number.");
-        }
-        else
-        {
-            Console.WriteLine("Too high! Try guessing a lower number.");
-        }
-    }
-}
-catch (FormatException ex)
-{
-    Console.WriteLine("Error: Invalid input. Please enter a number. " + ex.Message);
+    Console.Write("Enter the second string: ");
+    string secondString = Console.ReadLine();
+
+    string result = ConcatenateStrings(firstString, secondString);
+
+    Console.WriteLine("Concatenated Result: " + result);
 }
 catch (Exception ex)
 {
-    Console.WriteLine("An unexpected error occurred: " + ex.Message);
+    Console.WriteLine("Error: " + ex.Message);
+}
+    
+
+    static string ConcatenateStrings(string str1, string str2)
+{
+    return str1 + str2;
 }
