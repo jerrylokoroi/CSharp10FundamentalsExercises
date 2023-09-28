93ebf94a6af7d3556adc759f896c555abdcfda73
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-09-28 23:15:32 +0300

    Write a program  that generates a random number between 1 and 100. Ask the user to guess the number. Use exception handling to catch any error that might occur while reading the user’s input. If the user guesses the number correctly, congratulate them. Otherwise, give them a hint (higher or lower) and ask them to guess again.

--- CSharp10FundamentalsExercises/Program.cs
+++ CSharp10FundamentalsExercises/Program.cs
@@ -1,25 +1,33 @@
-﻿using CSharp10FundamentalsExercises;
+﻿Random random = new Random();
+int targetNumber = random.Next(1, 101);
+
+bool guessedCorrectly = false;
 
 try
 {
-    Console.Write("Enter a positive number: ");
-    int userInput = Convert.ToInt32(Console.ReadLine());
-
-    if (userInput < 0)
+    while (!guessedCorrectly)
     {
-        throw new NegativeNumberException("Input is a negative number.");
-    }
+        Console.Write("Guess the number (between 1 and 100): ");
+        int userGuess = Convert.ToInt32(Console.ReadLine());
 
-    double squareRoot = Math.Sqrt(userInput);
-    Console.WriteLine($"Square root of {userInput} is: {squareRoot}");
+        if (userGuess == targetNumber)
+        {
+            guessedCorrectly = true;
+            Console.WriteLine("Congratulations! You guessed the number.");
+        }
+        else if (userGuess < targetNumber)
+        {
+            Console.WriteLine("Too low! Try guessing a higher number.");
+        }
+        else
+        {
+            Console.WriteLine("Too high! Try guessing a lower number.");
+        }
+    }
 }
 catch (FormatException ex)
 {
-    Console.WriteLine("Error: Invalid input. " + ex.Message);
-}
-catch (NegativeNumberException ex)
-{
-    Console.WriteLine("Error: " + ex.Message);
+    Console.WriteLine("Error: Invalid input. Please enter a number. " + ex.Message);
 }
 catch (Exception ex)
 {
